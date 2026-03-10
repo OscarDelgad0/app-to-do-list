fbae39850d0cb1796409a878cf0af9ddca82a6e8
Oscar DL <jerry.dev.tec@hotmail.com>
2026-03-10 13:28:08 -0600

    [OGDL][feat]: Refactorización de función getTodo

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,4 +1,5 @@
 import prisma from '@/app/lib/prisma';
+import { Todo } from '@prisma/client';
 import { NextResponse, NextRequest } from 'next/server'
 import * as yup from 'yup';
 
@@ -8,6 +9,15 @@ interface Segments {
     }
 }
 
+const getTodo = async (id: string):Promise<Todo | null> => {
+
+    const todo = await prisma.todo.findFirst({where: {id}});
+    return todo;
+
+   
+}
+
+
 const putSchema = yup.object({
     complete: yup.boolean().optional(),
     description: yup.string().optional(),
@@ -15,9 +25,9 @@ const putSchema = yup.object({
 })
 
 export async function GET(request: Request, {params}: Segments) { 
-
+ 
     const { id } = params;
-    const todo = await prisma.todo.findFirst({where: {id}})
+    const todo = await getTodo(id);
 
         if(!todo){
         return NextResponse.json({message: `Todo con id ${ id } no existe`}, {status: 404})
@@ -28,7 +38,7 @@ export async function GET(request: Request, {params}: Segments) {
 export async function PUT(request: Request, {params}: Segments) { 
 
     const { id } = params;
-    const todo = await prisma.todo.findFirst({where: {id}})
+    const todo = await getTodo(id);
 
     if(!todo){
         return NextResponse.json({message: `Todo con id ${ id } no existe`}, {status: 404})
@@ -43,7 +53,7 @@ export async function PUT(request: Request, {params}: Segments) {
         })
 
         return NextResponse.json(updatedTodo)
-        
+
     }catch(error) {
         return NextResponse.json(error, {status: 400})
     }
